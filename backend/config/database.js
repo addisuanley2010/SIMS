f94f615f0fed8922fde648506919b7e1e52e4981
Adda <addisuanley2010@gmail.com>
2025-01-18 11:31:59 +0300

    database code updated

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,23 @@
+// const mysql = require('mysql2');
+
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'SIMS'
+// });
+
+// module.exports = db;
+
+
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'SIMS'
+    host: 'beroogxcnmmnckxglpqc-mysql.services.clever-cloud.com',
+    user: 'u3lelttq538reowu',
+    password: 'XaIa5goSIJJHXTwTg91l',
+    database: 'beroogxcnmmnckxglpqc'
 });
 
 module.exports = db;
